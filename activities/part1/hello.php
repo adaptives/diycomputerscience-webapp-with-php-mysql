175a4b975f9aca82c9c4e590b4dbf458b4324396
Parag Shah <adaptives@gmail.com>
2011-06-04 21:47:41 +0530

    removed unnecassary directories

--- activities/part1/hello.php
+++ activities/part1/hello.php
@@ -1,3 +1,4 @@
+<h2>Activity 1</h2>
 <?php
   echo '<b>Hello Your Name Goes Here</b>'
 ?>
--- activities/part1/helloat.php
+++ activities/part1/helloat.php
@@ -1,3 +1,4 @@
+<h2>Extra example to show how to add some dynamism to PHP code</h2>
 <?php
   echo '<b>Hello</b> the time right now is '.date('H:i, jS F');
 ?>
