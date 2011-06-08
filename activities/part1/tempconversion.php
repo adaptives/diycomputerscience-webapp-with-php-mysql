f97a8c848584703e1d76d4e7f4f8d351d5c53815
Parag Shah <adaptives@gmail.com>
2011-06-08 23:39:52 +0530

    added support to perform both way conversions

--- activities/part1/tempconversion.php
+++ activities/part1/tempconversion.php
@@ -1,15 +1,29 @@
 <?php
 
-  $temp_in_fahrenheit = $_POST['temp'];
-  if($temp_in_fahrenheit != NULL) {
-    $celcius = (5/9) * ($temp_in_fahrenheit - 32);
-    echo "<div> $temp_in_fahrenheit Fahrenheit is $celcius Celcius. </div>";
+  $temp = $_POST['temp'];
+  if($temp != NULL) {
+    $curr_metric;
+    $converted_metric;
+    $converted_temp;
+
+    if($_POST['temp_metric'] == 'fahrenheit') {
+      $curr_metric = "Fahrenheit";
+      $converted_metric = "Celcius";
+      $converted_temp = (5/9) * ($temp - 32);
+    } else {
+      $curr_metric = "Celcius";
+      $converted_metric = "Fahrenheit";
+      $converted_temp = $temp * 9/5 + 32;
+    }
+    echo "<div> $temp $curr_metric is $converted_temp $converted_metric. </div>";
   }
   
   echo '<div>';
   echo '<form method="POST" action="">';
     echo "<div>";
       echo '<input type="text" name="temp" />';
+      echo '<input type="radio" name="temp_metric" value="fahrenheit" /> Fahrenheit';
+      echo '<input type="radio" name="temp_metric" value="celcius" /> Celcius';
     echo "</div>";
     echo "<div>";
       echo '<input type="submit" name="submit" value="submit" />';
